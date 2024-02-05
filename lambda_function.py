2561da4cce9262b78bb09020c51c5c2db803a6ce
satory074 <satory074@gmail.com>
2024-02-06 00:58:55 +0900

    feat: neko

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,12 @@
 import json
 import os
 
+import requests
 from nacl.exceptions import BadSignatureError
 from nacl.signing import VerifyKey
 
 
 def lambda_handler(event, context):
-    print(event)
     PUBLIC_KEY = os.environ['PUBLIC_KEY']
 
     verify_key = VerifyKey(bytes.fromhex(PUBLIC_KEY))
@@ -15,22 +15,37 @@ def lambda_handler(event, context):
     timestamp = event['headers']["x-signature-timestamp"]
     body = event['body']
 
-    print(body)
+    print('event:', event)
+    print('body', body)
 
     try:
         verify_key.verify(f'{timestamp}{body}'.encode(), bytes.fromhex(signature))
         body = json.loads(event['body'])
-        print('body', body)
+
         if body["type"] == 1:
             return {
                 'statusCode': 200,
                 'body': json.dumps({'type': 1})
             }
+
+        if body["type"] == 2:
+            url = "https://discord.com/api/v10/interactions/{}/{}/callback".format(body['id'], body['token'])
+
+            data = {
+                "type": 4,
+                "data": {
+                    "content": "にゃーん"
+                }
+            }
+
+            r = requests.post(url, json=data)
+            print('r:', r.json())
+            return
     except (BadSignatureError) as e:
         return {
             'statusCode': 401,
             'body': json.dumps("Bad Signature")
-        }
+            }
 
     return {
         'statusCode': 404
